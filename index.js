447bf677cc9fc61c9afec39232b72d6b4a5abeb8
aphinan <apinun16159@gmail.com>
2024-10-05 14:40:04 +0700

    install

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+
+const { Reader } = require('thaismartcardreader.js');
+
+
 const PORT = process.env.PORT;
+
+const myReader = new Reader();
+
+myReader.on('device-activated', () => {
+    console.log('อุปกรณ์ถูกเปิดใช้งาน รอการเสียบบัตร...');
+    isDeviceConnected = true;
+});
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// Sample data
+// Sample datagit 
 let items = [
     { id: 1, name: 'Item 1' },
     { id: 2, name: 'Item 2' },
